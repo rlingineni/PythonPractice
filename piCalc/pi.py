4de1ea7d70e1fcb6e1ac577da337fe20395271a0
rlingineni <Raviteja@Ravitejas-MacBook-Pro.local>
2015-09-10 10:44:39 -0500

    python piRounder

---
+++ piCalc/pi.py
@@ -0,0 +1,21 @@
+import math 
+
+def CalculatePi(roundVal):
+
+		somepi = round(math.pi,roundVal);
+		pi = str(somepi)
+		someList = list(pi)
+		return somepi;
+	
+	
+
+
+
+roundTo = raw_input('Enter the number of digits you want after the decimal for Pi: ')
+try:
+	roundint = int(roundTo);
+	print CalculatePi(roundint);
+except:
+	print "You did not enter an integer";
+
+
